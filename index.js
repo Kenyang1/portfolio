086cda22d9d85851e75ef1641b49655ef339c229
Kenyang <kenyanglual05@gmail.com>
2025-05-08 23:21:35 -0400

    pikachu sound effect

--- index.js
+++ index.js
@@ -4,14 +4,29 @@ document.addEventListener("DOMContentLoaded", function () {
 
   menuIcon.addEventListener("click", () => {
     if (mobileMenu.classList.contains("active")) {
-      // Start fade-out
       mobileMenu.classList.remove("active");
       mobileMenu.classList.add("closing");
       setTimeout(() => {
         mobileMenu.classList.remove("closing");
-      }, 320); // Match the CSS transition duration
+      }, 320); 
     } else {
       mobileMenu.classList.add("active");
     }
+  });
+
+  const pikachuImg = document.getElementById("pikachu-img");
+  const pikachuSound = document.getElementById("pikachu-sound");
+
+  pikachuImg.addEventListener("click", () => {
+
+
+
+    pikachuSound.currentTime = 0;
+    pikachuSound.play();
+    
+    pikachuImg.style.transform = "scale(1.1)";
+    setTimeout(() => {
+      pikachuImg.style.transform = "scale(1)";
+    }, 200);
   });
 });
